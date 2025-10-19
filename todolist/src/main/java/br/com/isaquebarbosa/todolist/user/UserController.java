961d962d16b2be5b84ebfc275727a6e27bf0e893
IsaqueSouzaa <isaquebarbosa463@gmail.com>
2025-10-19 13:22:02 -0300

    Criptografei a senha no banco de dados e inserir um codigo de erro

--- todolist/src/main/java/br/com/isaquebarbosa/todolist/user/UserController.java
+++ todolist/src/main/java/br/com/isaquebarbosa/todolist/user/UserController.java
@@ -1,6 +1,10 @@
 package br.com.isaquebarbosa.todolist.user;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,10 +18,21 @@ public class UserController {
     private IUserRepository userRepository;
 
     @PostMapping("/")
-    public UserModel create(@RequestBody UserModel userModel){
+    public ResponseEntity create(@RequestBody UserModel userModel){
+       var user = this.userRepository.findByUsername(userModel.getUsername());
+
+       if(user != null){
+           // Mensagem de erro
+           // Status Code
+           return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+       }
+       
+        var passwordHashred = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+       userModel.setPassword(passwordHashred);
 
        var userCreated = this.userRepository.save(userModel);
-       return userCreated;
+       return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 
 }
